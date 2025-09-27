49fab91d1605b2ef72a1e2c9f514f33ee94e449f
kanatuly-m <240941@astanait.edu.kz>
2025-09-27 17:23:44 +0500

    metrics

--- src/main/java/algorithms/Kadane.java
+++ src/main/java/algorithms/Kadane.java
@@ -1,4 +1,42 @@
 package algorithms;
 
+import metrics.PerformanceTracker;
+
 public class Kadane {
+    public static Result kadane(int[] a, PerformanceTracker t) {
+        if (a == null || a.length == 0) {
+            throw new IllegalArgumentException("Array must not be null or empty");
+        }
+
+        t.startTimer();
+
+        int bestSum = a[0]; t.incArrayAccesses();
+        int curSum  = a[0]; t.incArrayAccesses();
+        int bestL = 0, bestR = 0, curL = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            t.incComparisons();                  // i < a.length
+            int x = a[i]; t.incArrayAccesses();  // чтение a[i]
+
+            // начать новый подмассив или продолжить текущий
+            t.incComparisons();
+            if (curSum + x < x) {
+                curSum = x; t.incAssignments();
+                curL = i;  t.incAssignments();
+            } else {
+                curSum = curSum + x; t.incAssignments();
+            }
+
+            // обновить лучший ответ
+            t.incComparisons();
+            if (curSum > bestSum) {
+                bestSum = curSum; t.incAssignments();
+                bestL = curL;     t.incAssignments();
+                bestR = i;        t.incAssignments();
+            }
+        }
+
+        t.stopTimer();
+        return new Result(bestSum, bestL, bestR);
+    }
 }
--- src/main/java/algorithms/Result.java
+++ src/main/java/algorithms/Result.java
@@ -1,4 +1,18 @@
 package algorithms;
 
 public class Result {
+    public final int maxSum;
+    public final int left;
+    public final int right;
+
+    public Result(int maxSum, int left, int right) {
+        this.maxSum = maxSum;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{sum=" + maxSum + ", L=" + left + ", R=" + right + "}";
+    }
 }
